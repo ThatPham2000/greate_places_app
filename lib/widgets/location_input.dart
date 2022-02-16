11a42a7db54ac0cd9c6eb72a56c12f225b19c308
PHAT <that.phamvan@elca.vn>
2022-02-16 12:06:48 +0700

    add location

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,77 @@
+import 'package:location/location.dart';
+import 'package:flutter/material.dart';
+import '../helpers/location_helper.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({Key? key}) : super(key: key);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    // print(locData.latitude);
+    // print(locData.longitude);
+
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: locData.latitude ?? 0, longitude: locData.longitude ?? 0);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              label: Text('Select on Map'),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
